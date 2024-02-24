9bdf8a1b4d6068381e32729aef7a48761b2526dc
Göksu Ayaz <goksuayaz79@gmail.com>
2024-02-24 11:24:31 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int i = 20;
+        double dbl = 2.99;
+        i = (int) dbl;
+
+        System.out.println("dbl = " + dbl);
+        System.out.printf("i = " + i);
+
+
+
+
+    }
+}
